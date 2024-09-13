f5c6c757d901a5e84d3fc478c3ea1f72b8b19ddf
Darnes1958 <abdel_ati_m@yahoo.com>
2024-09-14 00:52:02 +0200

    any

--- resources/views/filament/user/pages/job-data.blade.php
+++ resources/views/filament/user/pages/job-data.blade.php
@@ -1,7 +1,8 @@
 <div class="flex">
     @if($record->Job)
 
-            @if($record->Job->jobType->name!='القوات_المسلحة')
+            @if($record->Job->jobType->name!='القوات_المسلحة' )
+
                 <p >{{$record->Job->name}}&nbsp;</p>
 
             @endif
@@ -10,10 +11,15 @@
                 <p style="color: #fbbf24; ">{{$record->Job->name}}&nbsp;</p>
                 <x-filament::avatar
                     src="{{ asset('img/milatary.jpg') }}"
-
                     size="sm"
                 />
             @endif
+                @if($record->Job->jobType->name=='الداخلية')
+                    <x-filament::avatar
+                        src="{{ asset('img/aladel.jpeg') }}"
+                        size="sm"
+                    />
+                @endif
 
 
     @endif
