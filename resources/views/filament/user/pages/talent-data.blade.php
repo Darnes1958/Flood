628cf8ee9d9bac5775243deb399e14c479122ac5
Darnes1958 <abdel_ati_m@yahoo.com>
2024-09-27 20:54:53 +0200

    update

--- resources/views/filament/user/pages/talent-data.blade.php
+++ resources/views/filament/user/pages/talent-data.blade.php
@@ -3,14 +3,11 @@
         @foreach($record->VicTalent as $talent)
             @if($talent->talent->talentType->name=='مواهب')
                 <p >{{$talent->talent->name}}&nbsp;</p>
-
             @endif
-
             @if($talent->talent->talentType->name=='دارنس')
                 <p style="color: #fbbf24; ">{{$talent->talent->name}}&nbsp;</p>
                 <x-filament::avatar
                     src="{{ asset('img/darens.jpg') }}"
-
                     size="sm"
                 />
             @endif
@@ -18,7 +15,6 @@
                 <p style="color: #00bb00; ">{{$talent->talent->name}}&nbsp;</p>
                 <x-filament::avatar
                     src="{{ asset('img/afriky.jpg') }}"
-
                     size="sm"
                 />
             @endif
@@ -26,7 +22,6 @@
                     <p style="color: #9f1239; ">{{$talent->talent->name}}&nbsp;</p>
                     <x-filament::avatar
                         src="{{ asset('img/helal.jpg') }}"
-
                         size="sm"
                     />
                 @endif
@@ -34,7 +29,6 @@
                     <p style="color: #3f6212; ">{{$talent->talent->name}}&nbsp;</p>
                     <x-filament::avatar
                         src="{{ asset('img/kashaf.jpg') }}"
-
                         size="sm"
                     />
                 @endif
