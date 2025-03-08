3301549eaf95503262566969064caf84d488d590
Darnes1958 <abdel_ati_m@yahoo.com>
2025-03-08 23:57:04 +0200

    aema

--- app/Enums/talentType.php
+++ app/Enums/talentType.php
@@ -12,6 +12,7 @@ enum talentType: int implements HasLabel,HasColor
   case الافريقي = 3;
   case الهلال_الاحمر = 4;
   case الكشافة = 5;
+  case حغاظ_وأئمة = 6;
 
 
 public function getColor(): string|array|null
@@ -22,15 +23,21 @@ public function getColor(): string|array|null
         self::الهلال_الاحمر => 'danger',
         self::الكشافة => 'Fuchsia',
         self::مواهب => 'blue',
-
-
-
+        self::حغاظ_وأئمة => 'blue',
     };
 }
 
 
     public function getLabel(): ?string
   {
-    return $this->name;
+
+      return match ($this) {
+          self::مواهب => 'مواهب',
+          self::دارنس => 'دارنس',
+          self::الكشافة => 'الكشافة والمرشدات',
+          self::الافريقي => 'الافريقي',
+          self::الهلال_الاحمر => 'الهلال الاحمر',
+          self::حغاظ_وأئمة => 'حفاظ و أئمة وقيمين',
+      };
   }
 }
