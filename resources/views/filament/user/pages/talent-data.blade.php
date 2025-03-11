9362d76b121ee10c6a161053b54b506a317e531f
Darnes1958 <abdel_ati_m@yahoo.com>
2025-03-11 21:33:14 +0200

    any

--- resources/views/filament/user/pages/talent-data.blade.php
+++ resources/views/filament/user/pages/talent-data.blade.php
@@ -1,37 +1,10 @@
 <div class="flex">
     @if($record->VicTalent)
         @foreach($record->VicTalent as $talent)
-            @if($talent->talent->talentType->name=='مواهب')
-                <p >{{$talent->talent->name}}&nbsp;</p>
-            @endif
-            @if($talent->talent->talentType->name=='دارنس')
-                <p style="color: #fbbf24; ">{{$talent->talent->name}}&nbsp;</p>
-                <x-filament::avatar
-                    src="{{ asset('img/darens.jpg') }}"
-                    size="sm"
-                />
-            @endif
-            @if($talent->talent->talentType->name=='الافريقي')
-                <p style="color: #00bb00; ">{{$talent->talent->name}}&nbsp;</p>
-                <x-filament::avatar
-                    src="{{ asset('img/afriky.jpg') }}"
-                    size="sm"
-                />
-            @endif
-                @if($talent->talent->talentType->name=='الهلال_الاحمر')
-                    <p style="color: #9f1239; ">{{$talent->talent->name}}&nbsp;</p>
-                    <x-filament::avatar
-                        src="{{ asset('img/helal.jpg') }}"
-                        size="sm"
-                    />
-                @endif
-                @if($talent->talent->talentType->name=='الكشافة')
-                    <p style="color: #3f6212; ">{{$talent->talent->name}}&nbsp;</p>
-                    <x-filament::avatar
-                        src="{{ asset('img/kashaf.jpg') }}"
-                        size="sm"
-                    />
-                @endif
+            <x-filament::avatar
+                src="{{  asset('storage/'.$talent->Talent->image) }} "
+                size="sm"
+            />
         @endforeach
     @endif
 
