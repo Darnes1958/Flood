76cd682a2545719a53aaa631113493b75f810c0f
Darnes1958 <abdel_ati_m@yahoo.com>
2025-08-17 23:00:24 +0200

    any

--- resources/views/filament/user/pages/view-father.blade.php
+++ resources/views/filament/user/pages/view-father.blade.php
@@ -1,6 +1,7 @@
 <div class="flex te text-lg">
+
     @if($record->sonOfFather)
-        @if($record->male=='ذكر')
+        @if(\App\Models\Victim::find($record->id)->male=='ذكر')
             <p style="color: #fbbf24;font-weight: bold">والده :&nbsp;</p>
         @else
           <p style="color: #fbbf24;font-weight: bold">والدها :&nbsp;</p>
